25341126264cf937e919780f36a6017d4610491b
Peter Barclay <peter@peter-barclay.com>
2019-10-13 09:24:46 +0100

    basic ui layout

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -5,8 +5,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let lengthOptions = ["meters", "km", "feet", "yards", "miles"]
+    @State private var inputSelected = 0
+    @State private var outputSelected = 0
+    @State private var inputValueString = ""
+
+    private var outputValueString: String {
+        return "123"
+    }
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("From", selection: $inputSelected) {
+                        ForEach(0 ..< lengthOptions.count) {
+                            Text("\(self.lengthOptions[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    TextField("Enter \(lengthOptions[inputSelected])", text: $inputValueString)
+                }
+                Section {
+                    Picker("To", selection: $outputSelected) {
+                        ForEach(0 ..< lengthOptions.count) {
+                            Text("\(self.lengthOptions[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("\(outputValueString)")
+                }
+            }
+        }
     }
 }
 
