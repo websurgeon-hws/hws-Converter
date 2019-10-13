54c9d683acce38c090d429bf9a24d90a001f08dc
Peter Barclay <peter@peter-barclay.com>
2019-10-13 09:52:37 +0100

    refactor to use native Measurement system

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -5,7 +5,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let lengthOptions = ["meters", "km", "feet", "yards", "miles"]
+    private let lengthOptions: [UnitLength] = [.meters, .kilometers, .feet, .yards, .miles]
     private let convertionFromMeters: [Double] = [1, 0.001, 3.28084, 1.09361, 0.000621371]
 
     @State private var inputSelected = 0
@@ -17,13 +17,22 @@ struct ContentView: View {
     }
 
     private var outputValue: Double {
-        let inputMeters = inputValue / convertionFromMeters[inputSelected]
-        
-        return inputMeters * convertionFromMeters[outputSelected]
+        let input = Measurement(value: inputValue, unit: lengthOptions[inputSelected])
+        return input.converted(to: lengthOptions[outputSelected]).value
     }
 
     private var outputValueString: String {
         return "\(outputValue)"
+    }
+    
+    private func nameForUnit(_ unit: UnitLength) -> String {
+        let formatter = MeasurementFormatter()
+        formatter.unitStyle = .long
+        return  formatter.string(from: unit)
+    }
+    
+    private func symbolForUnit(_ unit: UnitLength) -> String {
+        unit.symbol
     }
 
     var body: some View {
@@ -32,22 +41,22 @@ struct ContentView: View {
                 Section {
                     Picker("From", selection: $inputSelected) {
                         ForEach(0 ..< lengthOptions.count) {
-                            Text("\(self.lengthOptions[$0])")
+                            Text("\(self.symbolForUnit(self.lengthOptions[$0]))")
                         }
                     }.pickerStyle(SegmentedPickerStyle())
                 }
                 Section {
-                    TextField("Enter \(lengthOptions[inputSelected])", text: $inputValueString)
+                    TextField("Enter \(nameForUnit(lengthOptions[inputSelected]))", text: $inputValueString)
                 }
                 Section {
                     Picker("To", selection: $outputSelected) {
                         ForEach(0 ..< lengthOptions.count) {
-                            Text("\(self.lengthOptions[$0])")
+                            Text("\(self.symbolForUnit(self.lengthOptions[$0]))")
                         }
                     }.pickerStyle(SegmentedPickerStyle())
                 }
                 Section {
-                    Text("\(outputValueString)")
+                    Text("\(outputValueString) \(nameForUnit(self.lengthOptions[outputSelected]))")
                 }
             }
         }
